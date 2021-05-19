5db4616092592c37d3e3821cf479bedc0d7996e1
Nanyu <tny@nanyus-MacBook-Pro.local>
2021-05-19 18:37:59 +0800

    update files

--- C++面向对象高级编程(上)/complex.h
+++ C++面向对象高级编程(上)/complex.h
@@ -2,13 +2,14 @@
 #define __COMPLEX__
 
 #include <cmath>
-'''forward declarations'''
+
+'''0. forward declarations'''
 class ostream;
 class complex;
 
 complex& __doapl (complex* ths, const complex& r);
 
-'''class declarations'''
+'''1. class declarations'''
 class complex
 {
 public:
@@ -22,9 +23,114 @@ private:
 	friend complex& __doapl (complex*, const complex&);
 }
 
-inline double
-imag (const compplex& x)
+'''2. class definition类定义'''
+
+'''2.1'''
+inline complex&
+__doapl(complex* ths, const complex& r)
+
 {
-	return x.imag ()
+	ths -> re += r.re;
+	ths -> im += r.im;
+	return *ths;
+}
+'''操作符重载'''
+inline complex&
+complex::operator += (const complex& r)
+{
+	return __doapl (this, r);
+}
+
+'''2.2'''
+inline double
+imag (const complex& x)
+{
+	return x.imag ();
+}
+
+inline double
+real (const complex& x)
+{
+	return x.real ();
+}
+
+'''2.3'''
+inline complex
+operator + (const complex& x, const complex& y)
+{
+	return complex (real (x) + real (y), imag (x) + imag (y));
+}
+
+inline complex
+operator + (const complex& x, double y)
+{
+	reuturn complex (real (x) + y, imag (x));
+}
+
+inline complex
+operator + (double x, const complex& y)
+{
+	return complex (x + real (y), imag (y))
+}
+
+'''2.4'''
+inline complex
+operator + (const complex& x)
+{
+	return x;
+}
+
+inline complex
+operator - (const complex& x)
+{
+	return complex(-real (x), -imag (x));
+}
+
+'''2.5'''
+inline bool
+operator == (const complex& x, const complex& y)
+{
+	return real (x) == real (y) &&  imag (x) == imag (y);
+}
+inline bool
+operator == (const complex& x, double y)
+{
+	return real (x) == y && imag (x) == 0;
+}
+inline bool
+operator == (double x, const complex& y)
+{
+	return x == real (y) && imag (y) == 0;
+}
+
+'''2.6'''
+inline bool
+operator != (const complex& x, const complex& )
+{
+	return real (x) != real (y) ||  imag (x) != imag (y);
+}
+inline bool
+operator != (const complex& x, double y)
+{
+	return real (x) != y || imag (x) != 0;
+}
+inline bool
+operator != (double x, const complex& y)
+{
+	return x != real (y) || imag (y) != 0;
+}
+
+'''2.7'''
+inline complex
+conj (const complex& x)
+{
+	return complex (real (x), -imag (x));
+}
+
+#include <ostream.h>
+ostream&
+operator << (ostream& os, const complex& x)
+{
+	return os << '(' << real (x) << ',' << imag (x) << ')';
 }
 #endif
